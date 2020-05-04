17544247b9a4236c65a6aec58e14d2dae2397fcb
yinsweet <yinsweet@gmail.com>
2020-05-05 02:00:43 +0800

    Add navigation to top icon at bottom right

--- aden/js/main.js
+++ aden/js/main.js
@@ -7,6 +7,16 @@ $(document).ready(function(){
         $("#show-img").attr('src',img);
         $("#imgmodal").modal('show');
     });
+    
+    const toTop = document.querySelector(".to-top");
+
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 100) {
+            toTop.classList.add("active");
+        } else {
+            toTop.classList.remove("active");
+        }
+    })
 });
 
 function myNavFunc() {
